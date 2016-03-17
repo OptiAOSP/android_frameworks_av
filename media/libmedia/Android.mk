60b0533a5817372ae874c99d134e931afd4a2872
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-17 19:00:39 +0700

    libmedia: we don't support audio offload
    Change-Id: Id6efacd5bf3f66be128401bc159937ab6ee4e960

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES:= \
 LOCAL_MODULE:= libmedia_helper
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_C_FLAGS += -Werror -Wno-error=deprecated-declarations -Wall
+LOCAL_C_FLAGS += -Wno-error=deprecated-declarations -Wall
 LOCAL_CLANG := true
 
 include $(BUILD_STATIC_LIBRARY)
@@ -97,7 +97,7 @@ LOCAL_C_INCLUDES := \
     $(call include-path-for, audio-effects) \
     $(call include-path-for, audio-utils)
 
-LOCAL_CFLAGS += -Werror -Wno-error=deprecated-declarations -Wall
+LOCAL_CFLAGS += -Wno-error=deprecated-declarations -Wall
 LOCAL_CLANG := true
 
 include $(BUILD_SHARED_LIBRARY)
