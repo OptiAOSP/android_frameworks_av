7c22594e4b74a78ca336d01fab0d66514f8938da
Shilin Victor <chrono.monochrome@gmail.com>
2016-10-30 09:41:57 +0700

    Re-add OMXCodec
    Change-Id: Idb4464d52ff4f375676edf201dffc2626f6d2f66

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -47,6 +47,7 @@ LOCAL_SRC_FILES:=                         \
         NuCachedSource2.cpp               \
         NuMediaExtractor.cpp              \
         OMXClient.cpp                     \
+        OMXCodec.cpp                      \
         OggExtractor.cpp                  \
         ProcessInfo.cpp                   \
         SampleIterator.cpp                \
